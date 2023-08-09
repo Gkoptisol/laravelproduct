a9e791514ca38052e252212454b7cdf5c96d6785
Gopalakrishna B <gopalakrishna.b@optisolbusiness.com>
2023-08-09 20:32:34 +0530

    product create form

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<form method="POST" action="">
+        <div class="mt-2">
+          <label for="title" class="text-sm text-gray-500">Product Name</label>
+          <input id="title" type="text" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full">
+        </div>
+        <div class="mt-2">
+          <label for="desc-sm" class="text-sm text-gray-500">Unit Type</label> 
+          <select name="cars" id="cars" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full">
+                <option value="Qty">Qty</option>
+                <option value="Ltr">Ltr</option>
+                <option value="KG">KG</option>
+                <option value="Meter">Meter</option>
+        </select>
+        </div>
+        <div class="mt-2">
+          <label for="desc-full" class="text-sm text-gray-500">Product Category</label>
+          <input id="desc-full" type="text" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full">
+        </div>
+        <div class="mt-2">
+          <label for="price" class="text-sm text-gray-500">Product Images</label>
+          <input id="price" type="text" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 w-full">
+        </div>
+    </form>
